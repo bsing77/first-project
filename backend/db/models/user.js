e62fe9ffd21c0cffed0c2f5b09e37439ea0cc7b6
Brian Cabral <briansing77@gmail.com>
2023-06-26 22:46:15 -0400

    Add User signup backend endpoint

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,6 +1,6 @@
 'use strict';
 const {
-  Model
+  Model, Validator
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
       validate: {
         len: [4,30],
         isNotEmail(value){
-          if(validator.isEmail(value)){
+          if(Validator.isEmail(value)){
             throw new Error("Cannot be an email.");
           }
         }
