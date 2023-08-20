d1ef3fbec702a5ad9936428b89753dcbbfa51d38
Brian Cabral <briansing77@gmail.com>
2023-08-19 21:29:45 -0400

    added login and signup to drop down menu

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,35 +3,35 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
-import OpenModalButton from "../OpenModalButton";
-import LoginFormModal from "../LoginFormModal";
-import SignupFormModal from "../SignUpFormModal";
+// import OpenModalButton from "../OpenModalButton";
+// import LoginFormModal from "../LoginFormModal";
+// import SignupFormModal from "../SignUpFormModal";
 import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
 
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <li>
-        <ProfileButton user={sessionUser} />
-      </li>
-    );
-  } else {
-    sessionLinks = (
-      <li>
-        <OpenModalButton
-          buttonText="Log In"
-          modalComponent={<LoginFormModal />}
-        />
-        <OpenModalButton
-        buttonText = 'Sign Up'
-        modalComponent={<SignupFormModal/>}
-        />
-      </li>
-    );
-  }
+  // let sessionLinks;
+  // if (sessionUser) {
+  //   sessionLinks = (
+  //     <li>
+  //       <ProfileButton user={sessionUser} />
+  //     </li>
+  //   );
+  // } else {
+  //   sessionLinks = (
+  //     <li>
+  //       <OpenModalButton
+  //         buttonText="Log In"
+  //         modalComponent={<LoginFormModal />}
+  //       />
+  //       <OpenModalButton
+  //       buttonText = 'Sign Up'
+  //       modalComponent={<SignupFormModal/>}
+  //       />
+  //     </li>
+  //   );
+  // }
 
   return (
     <ul>
@@ -40,7 +40,11 @@ function Navigation({ isLoaded }) {
           Home
         </NavLink>
       </li>
-      {isLoaded && sessionLinks}
+      {isLoaded && (
+        <li>
+          <ProfileButton user = {sessionUser} />
+        </li>
+      )}
     </ul>
   );
 }
